43c35a78518fbde3afde62870be5dc97f70a31cd
adityabasude123 <adityabasude123@gmail.com>
2024-09-28 15:34:28 +0530

    add cors

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const connectToMongo = require("./utils/dbConnection");
-const cors = require('cors')
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 5000;
 connectToMongo();
 
-app.use(cors())
+app.use(
+  cors({
+    origin: "http://localhost:3000", // Allow requests from this origin
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  })
+);
 
 app.use(express.json());
 app.use("/api/product", require("./routes/productRoute"));
