19f9721420fd2ab3323e59db19b9298b8b48c937
adityabasude123 <adityabasude123@gmail.com>
2024-09-28 15:37:21 +0530

    update

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ connectToMongo();
 
 app.use(
   cors({
-    origin: "http://localhost:3000", // Allow requests from this origin
+    origin: "*", 
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   })
 );
